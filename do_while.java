b941d5db5f305646ecb815a3fa3473d3fe1fbabc
Krishna Jangid <108576349+Krishnaa234@users.noreply.github.com>
2023-04-17 23:05:45 +0530

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,9 @@
+public class do_while {
+    public static void main(String args[]) {
+        int i=0;
+        do {
+            System.out.print(i + " ");
+            i = i + 1;
+        } while(i<11);
+    }
+}
