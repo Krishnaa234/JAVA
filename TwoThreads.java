97c4eb6c5210699d74091ce240a16dc41aab192e
Krishna Jangid <108576349+Krishnaa234@users.noreply.github.com>
2023-09-05 22:40:13 +0530

    Create TwoThreads.java

---
+++ TwoThreads.java
@@ -0,0 +1,22 @@
+public class TwoThreads extends Thread{  
+    public void run(){  
+      
+    }  
+    public static void main(String args[]){  
+        TwoThreads t1=new TwoThreads();  
+        System.out.println("Name of thread 't1':"+ t1.getName());  
+    
+        TwoThreads t2=new TwoThreads();  
+        System.out.println("Name of thread 't2':"+ t2.getName());
+    
+        t1.start();  
+        t1.setName("Week 6 Assignment Q5");  
+    
+        t2.start(); 
+        t2.setName("Week 6 Assignment Q5 New");  
+    
+        System.out.println("New name of thread 't1':"+ t1.getName()); 
+        System.out.println("New name of thread 't2':"+ t2.getName());
+     
+     }  
+}
